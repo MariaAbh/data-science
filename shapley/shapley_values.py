8807d641a6b0ab5b285b3b47e94f7b380713397a
MariaAbh <maria.abouhaidar1@gmail.com>
2024-01-18 18:39:43 +0100

    replace data_sample with data_set word

--- shapley/shapley_values.py
+++ shapley/shapley_values.py
@@ -1,11 +1,14 @@
+def shapley_value_single(data_set,model,feature_index,sample_index):
+    prediction = model(sample)
 
-def shapley_value(data_sample, model, *, feature_index = None, sample_index = None):
+
+
+def shapley_value(data_set, model, *, feature_index = None, sample_index = None):
     if feature_index == None and sample_index == None:
-        return shapley_value_all(data_sample,model)
+        return shapley_value_all(data_set,model)
     elif sample_index == None:
-        return shapley_value_all_samples(data_sample,model,feature_index)
+        return shapley_value_all_samples(data_set,model,feature_index)
     elif feature_index == None:
-        return shapley_value_all_features(data_sample,model,sample_index)
+        return shapley_value_all_features(data_set,model,sample_index)
     else:
-        return shapley_value_single(data_sample,model,feature_index,sample_index)
-
+        return shapley_value_single(data_set,model,feature_index,sample_index)
