945d5a9369b49d0da7ef93638cb60cc78d1408d5
MariaAbh <maria.abouhaidar1@gmail.com>
2024-01-18 18:27:18 +0100

    first function

--- shapley/shapley_values.py
+++ shapley/shapley_values.py
@@ -4,4 +4,8 @@ def shapley_value(data_sample, model, *, feature_index = None, sample_index = No
         return shapley_value_all(data_sample,model)
     elif sample_index == None:
         return shapley_value_all_samples(data_sample,model,feature_index)
+    elif feature_index == None:
+        return shapley_value_all_features(data_sample,model,sample_index)
+    else:
+        return shapley_value_single(data_sample,model,feature_index,sample_index)
 
