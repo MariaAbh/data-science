ef3e7e6b7d5f3271dc80e940bab2887274e40347
MariaAbh <maria.abouhaidar1@gmail.com>
2024-01-18 20:06:00 +0100

    implement shapley value for single feature and sample

--- shapley/shapley_values.py
+++ shapley/shapley_values.py
@@ -1,7 +1,21 @@
-def shapley_value_single(data_set,model,feature_index,sample_index):
-    prediction = model(sample)
+import random
 
-
+def shapley_value_single(data_set,model,feature_index,sample_index,coef=0.5):
+    original_sample = data_set[sample_index]
+    original_len = len(original_sample)
+    n = coef * (len(data_set) * 2**len(original_sample))
+    for i in range(n):
+        random_point = random.choice(data_set)
+        partition = [random.randint(0,1) for _ in range(original_len)]
+        partition[feature_index] = 0
+        x1 = [(p*r)+(o*(1-p)) for p,r,o in zip(partition,random_point,original_sample)]
+        x2 = x1[::]
+        x2[feature_index] = random_point[feature_index]
+        prediction_x1 = model(x1)
+        prediction_x2 = model(x2)
+        difference = prediction_x1 - prediction_x2
+        result += difference
+    return result
 
 def shapley_value(data_set, model, *, feature_index = None, sample_index = None):
     if feature_index == None and sample_index == None:
