96eb9ad1c922ab8a56be6eab8035d930dce7ebea
MariaAbh <maria.abouhaidar1@gmail.com>
2024-01-18 20:33:51 +0100

    implement shapley value for all samples

--- shapley/shapley_values.py
+++ shapley/shapley_values.py
@@ -23,6 +23,12 @@ def shapley_value_all_features(data_set,model,sample_index,coef=0.5):
         values.append(shapley_value_single(data_set,model,feature_index,sample_index,coef))
     return values
 
+def shapley_value_all_samples(data_set,model,feature_index):
+    nb_of_samples = len(data_set)
+    for sample_index in range(nb_of_samples):
+        result += shapley_value_single(data_set,model,feature_index,sample_index,coef)
+    return result/nb_of_samples
+
 def shapley_value(data_set, model, *, feature_index = None, sample_index = None):
     if feature_index == None and sample_index == None:
         return shapley_value_all(data_set,model)
